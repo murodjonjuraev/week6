e48340858d4365434b6fe608ba3be6f8ebe88e99
murodjonjuraev <murodjon.juraev1@gmail.com>
2020-09-03 21:31:20 -0400

    "Commiton read and write"

--- files_and_exeptionds.py
+++ files_and_exeptionds.py
@@ -1,1 +1,12 @@
-print("hello")
+file_name = 'data/inputData.txt'
+
+with open('data/inputData.txt') as input_data:
+    contents = input_data.read()
+    print(contents)
+
+print(contents)
+print("Reading a inputData.txt file completed")
+
+# writing to the file
+with open(file_name, 'a') as file_object:
+    file_object.write("I love programing.\n")
